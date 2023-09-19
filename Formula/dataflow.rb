7010d235170efa7dc882f88087f4a6b822d34b3d
Andrew Mackenzie <andrew@mackenzie-serres.net>
2023-09-19 12:22:19 +0200

    Update dataflow.rb
    Update to match latest version number

--- Formula/dataflow.rb
+++ Formula/dataflow.rb
@@ -6,7 +6,7 @@ class Dataflow < Formula
   homepage "https://github.com/andrewdavidmackenzie/flow"
   url "https://github.com/andrewdavidmackenzie/flow/archive/refs/tags/v0.130.0.tar.gz"
   sha256 "6e745180a9015d576fb774dfb04c776958cf040809592a6b3e165836fb041d99"
-  version "0.134.0"
+  version "0.135.0"
   
   depends_on "rust" => :build # Newlines surrounding `depends_on` is required.
 
