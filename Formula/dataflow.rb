742d8788315f093edd98c4a8c1c801b20c2e5f6c
Andrew Mackenzie <andrew@mackenzie-serres.net>
2023-09-26 18:54:59 +0200

    Fix comments

--- Formula/dataflow.rb
+++ Formula/dataflow.rb
@@ -10,23 +10,27 @@ class Dataflow < Formula
   
   depends_on "rust" => :build # Newlines surrounding `depends_on` is required.
 
-  # See INSTALLING.md in https://github.com/andrewdavidmackenzie/flow for more details on the need for
-  # nightly rust and how to ensure this can be built
   def install
     # Install cargo-binstall
-    curl -L --proto '=https' --tlsv1.2 -sSf https://raw.githubusercontent.com/cargo-bins/cargo-binstall/main/install-from-binstall-release.sh | bash
+    system "curl", "-L", "--proto", "'=https'", "--tlsv1.2", "-sSf", "https://raw.githubusercontent.com/cargo-bins/cargo-binstall/main/install-from-binstall-release.sh", "-o", "install-from-binstall-release.sh"
+    system "bash", "install-from-binstall-release.sh"
     
     # binstall the flowc binary
-    cargo binstall flowc
+    system "cargo",  "binstall", "flowc"
 
     # cargo binstall flowr crate's multiple binaries: flowrcli, flowrgui and flowrex
 
     # download the flowstdlib artifact and expand into $HOME/.flow/lib
-    curl -L -s https://github.com/andrewdavidmackenzie/flow/releases/download/v0.135.0/flowstdlib-v0.135.0.tar.xz | tar xvz - -C $HOME/.flow/lib
+    system "curl", "-L", "--proto", "'=https'", "--tlsv1.2", "-sSf", "https://github.com/andrewdavidmackenzie/flow/releases/download/v0.135.0/flowstdlib-v0.135.0.tar.xz", "-o", "flowstdlib-v0.135.0.tar.xz"
+    system "tar", "-xvz", "flowstdlib-v0.135.0.tar.xz", "-", "-C" "$HOME/.flow/lib"
 
-    # download the flowrcli and flowrgui contexts into $HOME/.flow/runner
-    curl -L -s https://github.com/andrewdavidmackenzie/flow/releases/download/v0.135.0/flowrcli-v0.135.0.tar.xz | tar xvz - -C $HOME/.flow/runner
-    curl -L -s https://github.com/andrewdavidmackenzie/flow/releases/download/v0.135.0/flowrgui-v0.135.0.tar.xz | tar xvz - -C $HOME/.flow/runner
+    # download the flowrcli context into $HOME/.flow/runner
+    system "curl", "-L", "--proto", "'=https'", "--tlsv1.2", "-sSf", "https://github.com/andrewdavidmackenzie/flow/releases/download/v0.135.0/flowrcli-v0.135.0.tar.xz", "-o", "flowrcli-v0.135.0.tar.xz"
+    system "tar", "-xvz", "flowrcli-v0.135.0.tar.xz", "-", "-C" "$HOME/.flow/lib"
+
+    # download the flowrgui context into $HOME/.flow/runner
+    system "curl", "-L", "--proto", "'=https'", "--tlsv1.2", "-sSf", "https://github.com/andrewdavidmackenzie/flow/releases/download/v0.135.0/flowrgui-v0.135.0.tar.xz", "-o", "flowrgui-v0.135.0.tar.xz"
+    system "tar", "-xvz", "flowrgui-v0.135.0.tar.xz", "-", "-C" "$HOME/.flow/lib"
 
   end
 end
